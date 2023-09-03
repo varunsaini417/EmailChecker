a10d93eeb67874548a45cabbd91b680c833ab4e8
varunsaini417 <varunsaini417@gmail.com>
2023-09-03 10:45:26 +0530

    first commit

---
+++ src/EmailChecker.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+public class EmailChecker {
+
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println("Enter the gmail to be checked");
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        
+        System.out.println("This email is about to verify: "+ str);
+        
+    }
+    
+}
