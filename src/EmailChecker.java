1971f26d7bae0792842654ae177f93acc8400c80
varunsaini417 <varunsaini417@gmail.com>
2023-09-03 11:04:44 +0530

    added all the features to check the gmail or not

--- src/EmailChecker.java
+++ src/EmailChecker.java
@@ -10,7 +10,12 @@ public class EmailChecker {
         str = sc.nextLine();
         
         System.out.println("This email is about to verify: "+ str);
-        
+        int i = str.indexOf("@");
+        String uName = str.substring(0, i);
+        String domain = str.substring(i+1, str.length());
+        System.out.println("The username is: "+ uName);
+        System.out.println("And the domain is : "+ domain);
+        System.out.println(domain.startsWith("gmail"));
     }
     
 }
